b27fa5a3f82dcfa299e1af6bff8dd99cc1b7f675
vishalDalavayi <100804883+vishalDalavayi@users.noreply.github.com>
2024-12-13 23:31:09 -0500

    Update about.js

--- public/about.js
+++ public/about.js
@@ -47,3 +47,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ctx = document.getElementById("workoutChart").getContext("2d");
+
+  const workoutChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Yoga", "Running", "Weightlifting", "Cycling", "Swimming"],
+      datasets: [
+        {
+          label: "Workout Preferences",
+         
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
